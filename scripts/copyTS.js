2af8b088864e118661fbb6c23cfabc77b6267d30
Briws <brightwiths@gmail.com>
2022-04-21 22:36:06 +0300

    added copy svg to copyTS.js

--- scripts/copyTS.js
+++ scripts/copyTS.js
@@ -5,13 +5,27 @@ const fse = require('fs-extra');
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 const srcDir = path.join('./src');
-const distDir = path.join('./dist/js');
+const jsDir = path.join('./dist/js');
+const esmDir = path.join('./dist/esm');
 
 const files = glob.sync('**/*.d.ts', {
     cwd: srcDir
 });
+
+const svgFiles = glob.sync('**/*.svg', {
+    cwd: srcDir
+});
+
 files.forEach(file => {
     const from = path.join(srcDir, file);
-    const to = path.join(distDir, file);
+    const to = path.join(jsDir, file);
     fse.copySync(from, to);
+});
+
+svgFiles.forEach(file => {
+    const from = path.join(srcDir, file);
+    const toJS = path.join(jsDir, file);
+    const toEsm = path.join(esmDir, file);
+    fse.copySync(from, toJS);
+    fse.copySync(from, toEsm);
 });
